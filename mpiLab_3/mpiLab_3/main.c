008b2b58dc02875407ad0cf9425233852172f548
ortin <michael.iq.00@gmail.com>
2021-04-23 18:52:43 +0300

    added mpiLab_3 folder

---
+++ mpiLab_3/mpiLab_3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mpiLab_3
+//
+//  Created by Mikhail Misharin on 23.04.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
